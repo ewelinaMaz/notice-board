d0618527a0b61e9404cc76178a56244203974949
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-07-31 19:20:16 +0200

    Add PostAdd component

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -8,10 +8,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Container from '@material-ui/core/Container';
-import Hidden from '@material-ui/core/Hidden';
+import FavoriteIcon  from '@material-ui/icons/Favorite';
 
 import clsx from 'clsx';
 
@@ -54,16 +51,16 @@ const Component = ({className, user}) => {
       <div className={clsx(className, styles.root)}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
+            <Button edge="start" startIcon= {<FavoriteIcon />} className={classes.menuButton} color="inherit" aria-label="menu">
+              My adds
+            </Button>
             <Typography variant="h6" className={classes.title}>
             Bulletin board
             </Typography>
           
             <Button color="inherit">
               <Link 
-                to='/login'
+                to='/'
                 className={classes.login}
               >Logout</Link>
             </Button>
@@ -76,9 +73,6 @@ const Component = ({className, user}) => {
       <div className={clsx(className, styles.root)}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" className={classes.title}>
             Bulletin board
             </Typography>
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
 /* selectors */
 export const getAll = ({posts}) => posts.data;
+
 export const getPostById = ({posts}, id) => {
-  const filteredPost = posts.data.filter(post => post._id == id);
+  const filteredPost = posts.data.filter(post => post._id === id);
   return filteredPost.length ? filteredPost[0] : { error: true };
 };
 /* action name creator */
@@ -12,13 +14,28 @@ const createActionName = name => `app/${reducerName}/${name}`;
 const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
+const ADD_POST = createActionName('ADD_POST');
+
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+export const addPost = payload => ({ payload, type: ADD_POST});
 
 /* thunk creators */
+
+export const addPostRequest = (post) => {
+  return async (dispatch) => {
+    dispatch(fetchStarted());
+    try {
+      let res = await axios.post('http://localhost:8000/api/posts/add', post);
+      dispatch(addPost(res));
+    } catch (e) {
+      dispatch(fetchError(e.message));
+    }
+  };
+};
 
 /* reducer */
 export const reducer = (statePart = [], action = {}) => {
@@ -48,6 +65,17 @@ export const reducer = (statePart = [], action = {}) => {
         loading: {
           active: false,
           error: action.payload,
+        },
+      };
+    }
+    case ADD_POST: {
+      return {
+        ...statePart,
+        data: [...statePart.data, {
+          ...action.payload}],
+        loading: {
+          active: false,
+          error: false,
         },
       };
     }
